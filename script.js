cf8b026ba196a8ac133c71c685427264365e6bb6
Aditya Singh <aditya.bsingh5@gmail.com>
2023-07-03 23:05:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = null;
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15 rem';
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // when there is no input
+  if (!guess) {
+    displayMessage('Error 404....');
+
+    // when player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    displayMessage('Correct Number!!');
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30 rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').value = highScore;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
+    //when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too High!' : 'Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lose, good day sir');
+      document.querySelector('.score').textContent = '0';
+    }
+  }
+});
